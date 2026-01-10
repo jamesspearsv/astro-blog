f44c5fa002da9b852de788efd4d4828a18adaade
James Spears <jamesspearsv@gmail.com>
2026-01-09 21:46:56 -0500

    vault backup: 2026-01-09 21:46:56

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,12 @@
 // import astro:content utilities
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 // import collection loader
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 
 // define Posts collection
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content' }),
+  loader: glob({ pattern: "**/*.md", base: "./blog-content" }),
   schema: z.object({
     title: z.string(),
     pubDate: z.coerce.date(),
