c71382e86621b654ca0e2d7e05379b39cd501685
James Spears <jamesspearsv@gmail.com>
2026-04-30 14:09:00 -0400

    Removed astro/alpinejs, astro/vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), alpinejs()]
+  integrations: [],
 });
