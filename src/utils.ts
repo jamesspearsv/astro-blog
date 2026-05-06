cfa5ee77f50e99c9ccff9648244f50a54dd29a16
James Spears <jamesspearsv@gmail.com>
2026-05-06 14:09:06 -0400

    Added filtering logic, tag linking to BlogList component

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,27 @@
 import { getCollection } from "astro:content";
 
-export async function getPosts() {
-  const posts = await getCollection("posts");
-  return posts.filter((post) => post.data.published);
+export async function getPosts(tag?: string) {
+  const posts = (await getCollection("posts")).filter((post) =>
+    tag
+      ? post.data.published && post.data.tags.includes(tag)
+      : post.data.published,
+  );
+
+  return posts.sort(
+    (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime(),
+  );
+}
+
+export async function getAvailableTags() {
+  const posts = await getPosts();
+
+  // Parse tags from post frontmatters
+  const tags: string[] = [];
+  for (const post of posts) {
+    for (const tag of post.data.tags) {
+      if (!tags.includes(tag)) tags.push(tag);
+    }
+  }
+
+  return tags;
 }
