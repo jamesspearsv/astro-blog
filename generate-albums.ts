172b329936e54ba75077d2808db76c15a51e3190
James Spears <jamesspearsv@gmail.com>
2026-05-20 21:21:51 -0400

    Added a script to transform images into webp format

--- generate-albums.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs/promises";
-
-const dir = process.argv[2];
-
-const contents = await fs.readdir(`/Users/james/Downloads/${dir}`);
-
-console.log(dir);
-console.log(contents);
