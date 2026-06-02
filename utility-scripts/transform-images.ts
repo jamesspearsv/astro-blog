43a8ccadd2a6f823a819917aa8ab41eb58d0eeb1
James Spears <jamesspearsv@gmail.com>
2026-06-01 22:02:13 -0400

    Updated transform-images.ts script

--- utility-scripts/transform-images.ts
+++ utility-scripts/transform-images.ts
@@ -2,35 +2,46 @@
  * Usage: node transform-images.ts [inputDir]
  */
 
-import sharp from "sharp";
-import * as fs from "fs/promises";
+import sharp from 'sharp';
+import * as fs from 'fs/promises';
 
 const inputPath = process.argv[2];
 
 if (!inputPath) {
-  console.log("MISSING REQUIRED ARGUMENTS");
-  console.log("Usage => `node transform-images.ts [input]`");
-  process.exit(1);
+    console.log('MISSING REQUIRED ARGUMENTS');
+    console.log('Usage => `node transform-images.ts [input]`');
+    process.exit(1);
 }
 
-console.log("Reading directory contents...\n");
+console.log('Reading directory contents...\n');
 const dirContents = await fs.readdir(inputPath);
 
-await fs.mkdir(inputPath + "/webp");
+await fs.mkdir(inputPath + '/webp');
 
-console.log("Transforming images...\n");
+console.log('Transforming images...\n');
 for (const item of dirContents) {
-  if (item !== ".DS_Store" && item.split(".")[1] !== "webp") {
-    try {
-      await sharp(`${inputPath}/${item}`)
-        .webp()
-        .toFile(`${inputPath}/webp/${item.split(".")[0]}.webp`);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
+    if (item !== '.DS_Store' && item.split('.')[1] !== 'webp') {
+        console.log('Processing ' + item);
+
+        try {
+            await sharp(`${inputPath}/${item}`)
+                .resize({ width: 400, withoutEnlargement: true })
+                .webp()
+                .toFile(`${inputPath}/webp/${item.split('.')[0]}-400w.webp`);
+
+            console.log('Successfully processed 400px width');
+
+            await sharp(`${inputPath}/${item}`)
+                .resize({ width: 1200, withoutEnlargement: true })
+                .webp()
+                .toFile(`${inputPath}/webp/${item.split('.')[0]}-1200w.webp`);
+
+            console.log('Successfully processed 1200px width');
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
     }
-    console.log(`${item.split(".")[0]}.webp`);
-  }
 }
 
-console.log("\nSuccess!");
+console.log('\nSuccess!');
